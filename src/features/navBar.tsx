858e88d47d01fb3a1277935810a6d711e69509c3
Noam Muallem <noam.muallem@collab.lendbuzz.com>
2024-08-04 16:14:50 +0300

    code readability and sticky header

--- src/features/navBar.tsx
+++ src/features/navBar.tsx
@@ -14,7 +14,7 @@ export const NavBar = () => {
     };
 
     return (
-        <nav className="bg-gray-800">
+        <nav className="sticky top-0 bg-gray-800 z-10">
             <div className="mx-auto px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
